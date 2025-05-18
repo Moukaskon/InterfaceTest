9f1100f5c6ab01878587a210a900975b70412685
Moukaskon <moukaskon04@gmail.com>
2025-05-18 13:37:00 +0300

    Initialize

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void draw() {
+        System.out.println("Drawing a circle with radius " + radius);
+    }
+}
